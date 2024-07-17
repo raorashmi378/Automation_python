a9e5523599a80e336f31512fd506d4f785979418
raorashmi378 <raorashmi378@gmail.com>
2024-07-17 11:46:47 +0530

    with o/p

--- day1/lab1.py
+++ day1/lab1.py
@@ -1,6 +1,13 @@
 print("hello world")
-print(2+2)
-print(2-2)
-print(2*2)
-print(2/2)
-print(2%2)
+print(2 + 2)
+print(2 - 2)
+print(2 * 2)
+print(2 / 2)
+print(2 % 2)
+
+#hello world
+#4
+#0
+#4
+#1.0
+#0
